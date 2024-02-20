6974f91cfe9a283bb1e86c58e15c9df9d7c81d1d
Monroe Carr <7376314+soybeanscat@users.noreply.github.com>
2024-02-20 12:55:36 +0000

    Init

---
+++ script.js
@@ -0,0 +1,6 @@
+function calculate() {
+    var number1 = parseFloat(document.getElementById('number1').value);
+    var number2 = parseFloat(document.getElementById('number2').value);
+    var result = number1 + number2;
+    document.getElementById('result').textContent = result;
+}
